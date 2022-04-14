394ea4df36904ac0a235b7f178564c919bd0d518
Evgeniy Opryshko <>
2022-04-14 11:10:41 +0300

    Creating and Combining Views

---
+++ SwiftUI Essentials/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  SwiftUI Essentials
+//
+//  Created by Evgeniy Opryshko on 14.04.2022.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
