35bc804709ddd732d113a209d3c1bd28c86e0737
Evgeniy Opryshko <>
2022-04-14 11:00:36 +0300

    Hello, world!

---
+++ SwiftUI Essentials/SwiftUI_EssentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_EssentialsApp.swift
+//  SwiftUI Essentials
+//
+//  Created by Evgeniy Opryshko on 14.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_EssentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
